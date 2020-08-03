10137177f99299cbf3b37931b3c850a2ac2171ff
Loki1214 <sugimoto@cat.phys.s.u-tokyo.ac.jp>
2020-08-03 11:47:58 +0900

    Created shell scripts for CPU

---
+++ CPU/testing_zheev_cpu.sh
@@ -0,0 +1,13 @@
+#!/bin/sh                                                                                                      
+#SBATCH --get-user-env                                                                                         
+#SBATCH -p prawnew                                                                                             
+#SBATCH --mem=2000M                                                                                            
+#SBATCH --time=0-01:00:00                                                                                      
+
+mkdir -p $HOME/data/Diagonalization
+cd $HOME/data/Diagonalization
+
+OUTPUT=zheev_cpu_$(hostname).txt
+echo \#$(hostname) > $OUTPUT
+echo 'for n in $(seq 1 100);do /home/sugimoto/bin/Diagonalization/CPU/testing_zheev_cpu.out $((500*$n)) 1>/dev/null 2>$OUTPUT; done'
+for n in $(seq 1 100);do /home/sugimoto/bin/Diagonalization/CPU/testing_zheev_cpu.out $((500*$n)) 1>/dev/null; done 2>$OUTPUT 
