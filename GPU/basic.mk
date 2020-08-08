08f5fbe684b069404597708dd41620643cd2e0ea
Loki1214 <45707861+Loki1214@users.noreply.github.com>
2020-08-08 10:08:31 +0900

    Removed errors and warnings in GPU/basic.mk and GPU/testing_cheevd.cu

--- GPU/basic.mk
+++ GPU/basic.mk
@@ -1,9 +1,10 @@
+CC = nvcc
 SRC_EXT = .cu
 OUT_EXT = .out
-CFLAGS  = -std=c11 -Wall -O2
+CFLAGS  = -O2 -DADD_ -arch=compute_75
 DEBAG_FLAG = -g
 CFLAGS += $(DEBAG_FLAG)
-LIBES = -lm -llapack -lblas
+LIBES = -lm -lmagma -llapack -lblas -L$(HOME)/magma/lib
 HEADER_DIR = ../Headers
 
 ALL_O  = $(wildcard ${HEADER_DIR}/*.o)
@@ -11,7 +12,9 @@ ALL_H  = $(wildcard ${HEADER_DIR}/*.h)
 
 TARGET = testing_cheevd.out
 EXECS = ${TARGET:.out=${OUT_EXT}}
-SOURCES = ${TARGET:.out=.c}
+SOURCES = ${TARGET:.out=${SRC_EXT}}
+
+.SUFFIXES: .cu
 
 all : ${FRC}
 	${MAKE} "CFLAGS=${CFLAGS}" "FRC=${FRC}" ${EXECS}
@@ -19,9 +22,12 @@ all : ${FRC}
 Makefile : ${FRC}
 	cp basic.mk $@
 	@echo '# Automatically-generated dependencies list:' >> $@
-	@${CC} ${CFLAGS} -MM ${SOURCES} >> $@
+	${CC} ${CFLAGS} -MM ${SOURCES} >> $@
 
-.o.out:
+.cu.out:
 	${CC} ${CFLAGS} -o $@ $< ${ALL_O} ${LIBES}
+
+.cu.o:
+	${CC} ${CFLAGS} -o $@ $< ${LIBES}
 
 force_rebuild :
--- GPU/testing_cheevd.cu
+++ GPU/testing_cheevd.cu
@@ -28,7 +28,7 @@ __global__ void setRandomMatrix(int Dmat, magmaFloatComplex* mat, curandStateMtg
     float rand1 = curand_normal(&MTGPStates_d[0]) /sqrt(2.0);
     float rand2 = curand_normal(&MTGPStates_d[0]) /sqrt(2.0);
     if(index1 == index2) mat[index1 +Dmat*index2] = MAGMA_C_MAKE(sqrt(2.0)*rand1,0);
-    else if(i < j) {
+    else if(index1 < index2) {
       mat[index1 +Dmat*index2] = MAGMA_C_MAKE(rand1,rand2);
       mat[index2 +Dmat*index1] = MAGMA_C_CONJ(mat[index1 +Dmat*index2]);
     }
@@ -41,11 +41,11 @@ int main(int argc, char **argv) {
       exit(EX_USAGE);
     }
     void (*funcPtr)(int, magmaFloatComplex*, curandStateMtgp32_t*, int);
-    funcPtr = SetRandomMatrix;
+    funcPtr = setRandomMatrix;
     struct cudaFuncAttributes attr;
     cudaFuncGetAttributes(&attr, funcPtr);
-    printf("constSizeBytes     = %d\n", attr.constSizeBytes);
-    printf("maxThreadsPerBlock = %d\n", attr.maxThreadsPerBlock);
+    printf("#constSizeBytes     = %zu\n", attr.constSizeBytes);
+    printf("#maxThreadsPerBlock = %d\n", attr.maxThreadsPerBlock);
 
     const int Dmat = atoi(argv[1]);
     int nThread = attr.maxThreadsPerBlock;
@@ -61,22 +61,22 @@ int main(int argc, char **argv) {
     mtgp32_kernel_params *KernelParams_d;
 
     magma_init();
-    magmaFloatComplex *mat_d, *EigenVectors_d, *temp_d;
+    magmaFloatComplex *mat_d = NULL, *EigenVectors_d = NULL, *temp_d;
     magmaFloatComplex alpha = MAGMA_C_MAKE(1,0);
     magmaFloatComplex beta  = MAGMA_C_MAKE(0,0);
-    float *EigenValues, *rwork, lrwork_t;
-    magmaFloatComplex *wA, *work, lwork_t;
+    float *EigenValues = NULL, *rwork, lrwork_t;
+    magmaFloatComplex *wA = NULL, *work, lwork_t;
     magma_int_t *iwork, liwork_t;
     magma_int_t lwork  = -1;
     magma_int_t lrwork = -1;
     magma_int_t liwork = -1;
     magma_int_t info;
-    magma_cheevd_gpu(MagmaVec, MagmaUpper, Dmat, EigenVectors_d, Dmat, w, wA, Dmat, &lwork_t, lwork, &lrwork_t, lrwork, &liwork_t, liwork, &info);
-    lwork  = lwork_t;
-    lrwork = lrwork_t;
+    magma_cheevd_gpu(MagmaVec, MagmaUpper, Dmat, EigenVectors_d, Dmat, EigenValues, wA, Dmat, &lwork_t, lwork, &lrwork_t, lrwork, &liwork_t, liwork, &info);
+    lwork  = (magma_int_t)MAGMA_C_REAL(lwork_t);
+    lrwork = (magma_int_t)lrwork_t;
     liwork = liwork_t;
 
-    magma_queue_t queue =  dNULL;
+    magma_queue_t queue = NULL;
     magma_int_t   dev   = 0;
 
     double start, end, T_diag, T_prod;
@@ -95,10 +95,10 @@ int main(int argc, char **argv) {
     cudaMalloc( (void**)&MTGPStates_d, nBlock*nBlock *sizeof(curandStateMtgp32) );
     cudaMalloc( (void**)&KernelParams_d, sizeof(mtgp32_kernel_params) );
     // ******************** (END)Allocation ******************** //
-
+ 
     curandMakeMTGP32Constants(mtgp32dc_params_fast_11213, KernelParams_d);
     curandMakeMTGP32KernelState(MTGPStates_d, mtgp32dc_params_fast_11213, KernelParams_d, 1, seed);
-    SetRandomMatrix<<<dimGrid,dimBlock>>>(Dmat, mat_d, MTGPStates_d, nBlock);
+    setRandomMatrix<<<dimGrid,dimBlock>>>(Dmat, mat_d, MTGPStates_d, nBlock);
     magma_cprint_gpu(Dmat, Dmat, mat_d, Dmat, queue);
     magma_ccopymatrix(Dmat, Dmat, mat_d, Dmat, EigenVectors_d, Dmat, queue);
 
