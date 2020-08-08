cb59f575bcf29db06986b8504d07c64824bf4e53
Loki1214 <45707861+Loki1214@users.noreply.github.com>
2020-08-08 11:49:59 +0900

    Fixed bug in configure.sh

--- configure.sh
+++ configure.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-if [ -n "$MAGMA_PATH" ]; then exit 0;
+echo "Searching for MAGMA library..."
+if [ -n "$MAGMA_PATH" ]; then exit 0;fi
 
+echo 'Executing "ldconfig -p | grep libmagma"...'
 if [ $(ldconfig -p | grep libmagma | wc -l) -gt 0 ]; then
-  cp basic.mk Makefile
-else
-  if [ $(locate libmagma | wc -l) -gt 0 ]; then
+    cp basic.mk Makefile
+    echo "Makefile was successfully created."
+    exit 0
+fi
+
+echo 'Executing "locate libmagma"...'
+if [ $(locate libmagma | wc -l) -gt 0 ]; then
     MAGMA=$(locate libmagma | head -n 1)
     MAGMA=${MAGMA%%/libmagma*}
     echo "MAGMA_PATH=-L${MAGMA}" > Makefile
-    echo basic.mk >> Makefile
-  else
-    COLOR_f=$$(printf '\033[36m');
-    COLOR_b=$$(printf '\033[m');
+    cat basic.mk >> Makefile
+    echo "Makefile was successfully created."
+else
+    COLOR_f=$(printf '\033[31m')
+    COLOR_b=$(printf '\033[m')
     echo "$${COLOR_f}Can't find MAGMA library. Please specify its location by setting MAGMA_PATH.$${COLOR_b}"
-  fi
 fi
